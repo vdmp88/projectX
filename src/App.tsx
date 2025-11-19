a9bb558ed0e570b38ed3391709a96615751302d7
Vadym Pakharuk <vadym.pakharuk@nixs.com>
2025-11-19 10:59:31 +0100

    feat: Setup tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useQuery } from '@tanstack/react-query'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['users'],
+    queryFn: async () => {
+      const response = await fetch(
+        'https://691c92533aaeed735c915ef6.mockapi.io/api/v1/users'
+      )
+      return response.json()
+    },
+  })
+
+  if (isLoading) return <div>Loading...</div>
+
+  if (error instanceof Error) return <div>Error: {error.message}</div>
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
   )
 }
 
